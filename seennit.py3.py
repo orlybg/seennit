909e0e1aa8a84220ac601e8acb08b97e3cdaefdb
rutherford <rutherford@clientsideweb.net>
2015-03-27 21:18:44 +0000

    script for py 3

--- seennit.py3.py
+++ seennit.py3.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 
-# https://github.com/rutherford/seennit
+# py 3 version tested on 3.4.2
+# requires praw, requests
 
 import praw
+# hide praw induced requests warnings
+import warnings
 
 # get user input, write inplace
 import getpass
@@ -19,12 +22,15 @@ import random
 # create dir
 import os
 
+# cli update
+import sys
+
 user = None
 passwd = None
-last =None
+last = None
 save_dir = None
 
-seennit = 'seennit.py'
+seennit = 'seennit.py3.py'
 skipped = 0
 
 def write_to_me(pattern, value):
@@ -40,7 +46,7 @@ def write_to_me(pattern, value):
 
 	t.seek(0)
 
-	o = open('seennit.py', 'w')
+	o = open( seennit, 'w')
 	for line in t:
 		o.write(line)
 	o.close()
@@ -49,6 +55,7 @@ def write_to_me(pattern, value):
 
 def download(s):
 	global skipped
+
 	if hasattr(s, 'url'):
 		# regex for image
 		url = s.url
@@ -61,7 +68,7 @@ def download(s):
 		match = img.search(url)
 		if match is not None:
 			# grab lone image
-			rand_name = [random.choice(string.lowercase) for x in range(7)]
+			rand_name = [random.choice(string.ascii_lowercase) for x in range(7)]
 
 			name = ''.join([''.join(rand_name), match.group(0)])
 			save_to_file(url, path, name )
@@ -69,19 +76,16 @@ def download(s):
 			# get ogp img urls
 			rq = requests.get(url)
 			ogp_urls = [ a[0] for a in imgur_imgs.findall(rq.text) ]
-			rand_name = [random.choice(string.lowercase) for x in range(7)]
+			rand_name = [random.choice(string.ascii_lowercase) for x in range(7)]
 			name = ''.join([''.join(rand_name), '_%d%s'])
 			for idx, u in enumerate(ogp_urls):
 				save_to_file(u, path, name % ( idx, img.search(u).group(0)))
 		else:
-			# try for video
-			
-
-			print 'unable to match url with a pattern: %s' % s.url
+			# print('unable to match url with a pattern: %s' % s.url)
 			save_line_to_file(url, save_dir, 'skipped.txt')
 			skipped +=1
 	else:
-		print 'no url attribute present'
+		print('unable to match submission with a pattern')
 
 def save_line_to_file(line, loc, name):
 	if not os.path.exists( loc ):
@@ -100,7 +104,7 @@ def save_to_file(url, loc, name, append=False):
 	with open('/'.join([loc,name]), write) as handle:
 		response = requests.get(url, stream=True)
 		if not response.ok:
-			print 'download %s failed' % url
+			print('download %s failed' % url)
 			save_line_to_file(url, save_dir, 'skipped.txt')
 			skipped +=1
 		for block in response.iter_content(1024):
@@ -110,18 +114,22 @@ def save_to_file(url, loc, name, append=False):
 
 if __name__ =='__main__':
 
+	warnings.simplefilter("ignore")
+
 	if user == None:
-		user = raw_input('Reddit username: ')
+		user = input('Reddit username: ')
 	if passwd == None:
 		passwd = getpass.getpass('password: ')
 
-	r = praw.Reddit('Seennit v1 - downloads saved image links from Reddit user account')
+	r = praw.Reddit('Seennit v2 - downloads saved image links from Reddit user account')
 
 	r.login(user, passwd)
 
 	if save_dir is None:
-		save_dir = raw_input('Enter base save dir: ')
+		save_dir = input('Enter base save dir: ')
 		write_to_me(r'(^save_dir =)', save_dir)
+
+	saves_to_do = []
 
 	params = {}
 	# ordered asc by user save date not submission date
@@ -129,7 +137,6 @@ if __name__ =='__main__':
 	more = True
 	top = 100
 	old_last = last
-	total = 0
 
 	while more: # while there is potentially more pages to fetch...
 		more = False # no more unless we hit top in inner loop...
@@ -141,20 +148,24 @@ if __name__ =='__main__':
 		for s in saves: # cycle through page...
 			if s.fullname == old_last: # if we hit the previous last processed, exit...
 				more = False
-				print 'reached last save point'
+				print('reached last save point')
 				break
-			download(s)
-			total+=1
-
+			saves_to_do.append(s)
 			count+=1
-			if total==1: # if record is first in page, record & set to new last record after page finished
-				first = s.fullname
-				write_to_me(r'(^last =)', first)
-
+			# download(s)
 		if count == top: # potentially more pages to fetch...
 			more = True
 		# one page finished, persist top last to file
 		#global last = first in file above
 		params['after'] = s.fullname # for new query take first record in newly processed page...
-	print 'total saves processed: %d' % total
-	print 'total attempted downloads missed: %d' % skipped
+	# cycle through saves from last, saving & updating one by one...
+	total = len(saves_to_do)
+	current = 0
+	for s in saves_to_do[::-1]:
+		current += 1
+		download(s)
+		write_to_me(r'(^last =)', s.fullname)
+		sys.stdout.write("\rprogress: %d/%d" % ( current, total))
+		sys.stdout.flush()
+	print('\rtotal saves processed: %d' % total)
+	print('total attempted downloads missed: %d' % skipped)
